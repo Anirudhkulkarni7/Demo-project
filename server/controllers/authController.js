1008590b26a3400a6e51eb9ad26dd13c571570c4
Aniruddh <kulkarnianiruddh50@gmail.com>
2025-01-21 15:05:46 +0530

    backend changes

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,20 +2,36 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
 
+// exports.loginUser = async (req, res) => {
+//   try {
+//     const { username, password } = req.body;
+//     const user = await User.findOne({ username });
+//     if (!user) {
+//       return res.status(401).json({ message: 'Invalid credentials' });
+//     }
+
+//     const match = await bcrypt.compare(password, user.password);
+//     if (!match) {
+//       return res.status(401).json({ message: 'Invalid credentials' });
+//     }
+
+//      res.json({ role: user.role });
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).json({ message: 'Server error' });
+//   }
+// };
+
+
+
 exports.loginUser = async (req, res) => {
   try {
     const { username, password } = req.body;
     const user = await User.findOne({ username });
-    if (!user) {
+    if (!user || user.password !== password) {
       return res.status(401).json({ message: 'Invalid credentials' });
     }
-
-    const match = await bcrypt.compare(password, user.password);
-    if (!match) {
-      return res.status(401).json({ message: 'Invalid credentials' });
-    }
-
-     res.json({ role: user.role });
+    res.json({ role: user.role });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: 'Server error' });
